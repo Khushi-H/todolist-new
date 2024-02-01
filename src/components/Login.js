2a619d55ac1377f267aff10535475a3318005005
Khushi-H <154000684+Khushi-H@users.noreply.github.com>
2024-02-01 10:38:19 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,10 +9,10 @@ const Login = () => {
     // const response = await fetch(
     //   "https://to-do-list-beta-gold.vercel.app/api/auth/login",
     //   {
-    const response = await fetch(
-      "https://todolist-updated.onrender.com/api/auth/login",
-      {
-        // const response = await fetch("http://localhost:5000/api/auth/login", {
+    // const response = await fetch(
+    //   "https://todolist-updated.onrender.com/api/auth/login",
+    //   {
+        const response = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
